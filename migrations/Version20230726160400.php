693ccc17a1a3505bc667646206a5233350653bff
ataslima <ataslima@linguaphone.fr>
2023-07-26 21:53:04 +0200

    Migration

--- migrations/Version20230726160400.php
+++ migrations/Version20230726160400.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230726134425 extends AbstractMigration
+final class Version20230726160400 extends AbstractMigration
 {
     public function getDescription(): string
     {
