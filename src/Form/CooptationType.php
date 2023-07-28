e70f87bdd1cc2af7d2a60b1a6b6ef3bdb8c26ca9
Manu1Mackenzii <emmanuelit29@gmail.com>
2023-07-28 17:04:51 +0200

    ajout de form cooptation

--- src/Form/CooptationType.php
+++ src/Form/CooptationType.php
@@ -9,15 +9,54 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class CooptationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                    bg-gray-100
+                    '
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                    bg-gray-100
+                    '
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    bg-gray-100
+                    focus:border-gray-500 focus:bg-white focus:ring-0',
+                    'placeholder' => 'john@example.com'
+                ]
+            ])
             ->add('cvFile', VichFileType::class, [
                 'label' => 'CV',
             ])
--- src/Form/OffersType.php
+++ src/Form/OffersType.php
@@ -6,15 +6,53 @@ use App\Entity\Offers;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class OffersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('address')
-            ->add('companyName')
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                    bg-gray-100
+                    '
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                    bg-gray-100
+                    '
+                ]
+            ])
+            ->add('companyName', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                    bg-gray-100
+                    '
+                ]
+            ])
         ;
     }
 
