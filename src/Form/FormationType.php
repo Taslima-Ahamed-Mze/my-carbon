7dd8cb7f272ef9c851184b5f33024bad782961ce
Manu1Mackenzii <emmanuelit29@gmail.com>
2023-07-28 17:53:04 +0200

    form event- formation

--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -14,9 +15,45 @@ class FormationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('formationUrl')
+            ->add('title', TextType::class, [
+                'label' => 'Nom de la formation',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                    bg-gray-100
+                    '
+                ]
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                    bg-gray-100
+                    '
+                ]
+            ])
+            ->add('formationUrl', TextType::class, [
+                'label' => 'Lien',
+                'attr' => [
+                    'class' => ' mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                    bg-gray-100
+                    '
+                ]
+            ])
             ->add('imageFile', VichImageType::class, [
                 'label' => 'Image de la formation',
             ])
